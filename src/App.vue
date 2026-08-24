6743a359db10e53c79c99af755f00ab929c62fb1
이현무 <mootwo0202@gmail.com>
2026-08-25 08:39:32 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import SampleOne from './components/practices/basic/SampleOne.vue'
+import SampleTwo from './components/practices/basic/SampleTwo.vue'
+
+import VueHtml from './components/practices/basic/VueHtml.vue'
+import VueHtmlXss from './components/practices/basic/VueHtmlXss.vue'
+import VueText from './components/practices/basic/VueText.vue'
+import VueBind from './components/practices/basic/VueBind.vue'
+import VueBindClass from './components/practices/basic/VueBindClass.vue'
+import VueBindStyle from './components/practices/basic/VueBindStyle.vue'
+import VueBindShorthand from './components/practices/basic/VueBindShorthand.vue'
+import VueIf from './components/practices/basic/VueIf.vue'
+import VueShow from './components/practices/basic/VueShow.vue'
+import VueFor from './components/practices/basic/VueFor.vue'
+import VuePre from './components/practices/basic/VuePre.vue'
+import VueCloak from './components/practices/basic/VueCloak.vue'
+import VueOnce from './components/practices/basic/VueOnce.vue'
+import VueMemo from './components/practices/basic/VueMemo.vue'
+</script>
+
+<template>
+  <div class="practice-container">
+    <h1>1. 학습환경구성</h1>
+    <hr />
+    <SampleOne />
+    <SampleTwo />
+    <h1>2. Vue Directive</h1>
+    <hr />
+    <VueHtml />
+    <VueHtmlXss />
+    <VueText />
+    <VueBind />
+    <VueBindClass />
+    <VueBindStyle />
+    <VueBindShorthand />
+    <VueIf />
+    <VueShow />
+    <VueFor />
+    <VuePre />
+    <VueCloak />
+    <VueOnce />
+    <VueMemo />
+  </div>
+</template>
+
+<style>
+@import '@/assets/practice.css';
+</style>
